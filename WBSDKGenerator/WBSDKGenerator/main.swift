206deacb67a88ef46762e8039df4282134c8e66b
xu.shuifeng <xushuifeng@xushuifengdeMacBook-Pro.local>
2018-04-11 15:17:54 +0800

    add initial project

---
+++ WBSDKGenerator/WBSDKGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WBSDKGenerator
+//
+//  Created by xu.shuifeng on 2018/4/11.
+//  Copyright © 2018 shuifeng.me. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
